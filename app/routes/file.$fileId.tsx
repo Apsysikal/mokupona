9545a314f6e94b6f181a255c8b04c0c9fad4774c
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2024-01-24 18:37:13 +0000

    Remove old file fetching code

--- app/routes/file.$fileId.tsx
+++ app/routes/file.$fileId.tsx
@@ -1,6 +1,3 @@
-import { readFile } from "fs/promises";
-import path from "node:path";
-
 import { LoaderFunctionArgs } from "@remix-run/node";
 import invariant from "tiny-invariant";
 
@@ -8,65 +5,15 @@ import { prisma } from "~/db.server";
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const { fileId } = params;
-  const fileFolder = process.env.IMAGE_UPLOAD_FOLDER;
 
-  invariant(
-    typeof fileFolder === "string",
-    "Variable IMAGE_UPLOAD_FOLDER must be set",
-  );
   invariant(typeof fileId === "string", "Parameter fileId must be provided");
 
-  const filePath = path.join(fileFolder, fileId);
-  const extension = await path.extname(filePath);
-  let file = null;
-  let content = null;
+  const file = await prisma.eventImage.findUnique({ where: { id: fileId } });
+  if (!file) throw new Response("Not found", { status: 404 });
+  const contentType = file.contentType;
+  const contentLength = file.blob.byteLength.toString();
 
-  let contentType = "";
-  let contentLength = "";
-
-  switch (extension) {
-    case ".jpg":
-      file = await readFile(filePath);
-      contentType = "image/jpeg";
-      contentLength = Buffer.byteLength(file.buffer).toString();
-      content = file.buffer;
-      break;
-
-    case ".jpeg":
-      file = await readFile(filePath);
-      contentType = "image/jpeg";
-      contentLength = Buffer.byteLength(file.buffer).toString();
-      content = file.buffer;
-      break;
-
-    case ".png":
-      file = await readFile(filePath);
-      contentType = "image/png";
-      contentLength = Buffer.byteLength(file.buffer).toString();
-      content = file.buffer;
-      break;
-
-    case ".webp":
-      file = await readFile(filePath);
-      contentType = "image/webp";
-      contentLength = Buffer.byteLength(file.buffer).toString();
-      content = file.buffer;
-      break;
-
-    case "":
-      // No extension. Look in the database
-      file = await prisma.eventImage.findUnique({ where: { id: fileId } });
-      if (!file) throw new Response("Not found", { status: 404 });
-      contentType = file.contentType;
-      contentLength = file.blob.byteLength.toString();
-      content = file.blob;
-      break;
-
-    default:
-      throw new Response("Not found", { status: 404 });
-  }
-
-  return new Response(content, {
+  return new Response(file.blob, {
     headers: {
       "Content-Type": contentType,
       "Content-Length": contentLength,
