0382953866c19e9397002fe849d3ce166ab1009d
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2024-03-11 20:25:27 +0000

    Remove unused remix utils dependencies

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverDependenciesToBundle: [/^remix-utils.*/],
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.test.{ts,tsx}"],
   serverModuleFormat: "cjs",
