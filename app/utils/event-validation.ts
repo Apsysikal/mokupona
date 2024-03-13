5c6306825227193b696cce16f5a093c6c3254a48
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2024-03-13 17:35:09 +0000

    Fix code-splitting problems with NodeOnDiskFile

--- app/utils/event-validation.ts
+++ app/utils/event-validation.ts
@@ -1,4 +1,3 @@
-import { NodeOnDiskFile } from "@remix-run/node";
 import { z } from "zod";
 
 export const EventSchema = z.object({
@@ -14,10 +13,7 @@ export const EventSchema = z.object({
     .min(0, "Price cannot be less than 0")
     .int(),
   cover: z
-    .union([
-      z.instanceof(File, { message: "You must select a file" }),
-      z.instanceof(NodeOnDiskFile, { message: "You must select a file" }),
-    ])
+    .instanceof(File, { message: "You must select a file" })
     .refine((file) => {
       return file.size !== 0;
     }, "You must select a file")
