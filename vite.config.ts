cb18f9e4d9131fbd0cea86ef9bffb59d62ced7eb
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2024-12-25 13:00:52 +0000

    Working on privacy policy display

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import mdx from "@mdx-js/rollup";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -12,6 +13,7 @@ declare module "@remix-run/server-runtime" {
 export default defineConfig({
   server: { port: 3000 },
   plugins: [
+    mdx(),
     remix({
       future: {
         v3_fetcherPersist: true,
