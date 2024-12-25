8d787300e6acba10ae29649a611a4bfcdb27a4e9
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2024-12-25 12:33:46 +0000

    Prevent users from signing up for past events

--- app/routes/dinners_.$dinnerId.tsx
+++ app/routes/dinners_.$dinnerId.tsx
@@ -80,6 +80,13 @@ export async function action({ params, request }: ActionFunctionArgs) {
 
   invariant(typeof dinnerId === "string", "Parameter dinnerId is missing");
 
+  const dinner = await getEventById(dinnerId);
+
+  if (!dinner) throw new Response("Not found", { status: 404 });
+  if (dinner.date < new Date()) {
+    throw new Response("Forbidden", { status: 400 });
+  }
+
   const formData = await request.formData();
   const submission = parseWithZod(formData, { schema });
 
@@ -138,155 +145,167 @@ export default function DinnerPage() {
   });
   const signupPerson = fields.signupPerson.getFieldset();
   const people = fields.people.getFieldList();
+  const isPastEvent = event.date < new Date();
 
   return (
     <main className="mx-auto mt-16 flex max-w-4xl grow flex-col gap-5 px-2 pb-8 pt-4">
       <DinnerView event={event} />
 
-      <div className="my-8 border" />
+      {isPastEvent ? null : (
+        <>
+          <div className="my-8 border" />
+          <Form
+            method="post"
+            {...getFormProps(form)}
+            className="flex flex-col gap-4"
+          >
+            {/**
+             * This button is needed as hitting Enter would otherwise remove the first person.
+             * https://github.com/edmundhung/conform/issues/216
+             */}
+            <button type="submit" hidden />
 
-      <Form
-        method="post"
-        {...getFormProps(form)}
-        className="flex flex-col gap-4"
-      >
-        {/**
-         * This button is needed as hitting Enter would otherwise remove the first person.
-         * https://github.com/edmundhung/conform/issues/216
-         */}
-        <button type="submit" hidden />
-
-        <ul className="flex flex-col gap-20">
-          <li className="flex gap-3">
-            <fieldset className="flex w-full flex-col gap-4">
-              <Field
-                labelProps={{ children: "Name" }}
-                inputProps={{
-                  ...getInputProps(signupPerson.name, { type: "text" }),
-                }}
-                errors={signupPerson.name.errors}
-              />
-
-              <Field
-                labelProps={{ children: "Email" }}
-                inputProps={{
-                  ...getInputProps(signupPerson.email, { type: "email" }),
-                }}
-                errors={signupPerson.email.errors}
-              />
-
-              <Field
-                labelProps={{ children: "Phone number" }}
-                inputProps={{
-                  ...getInputProps(signupPerson.phone, { type: "tel" }),
-                }}
-                errors={signupPerson.phone.errors}
-              />
-
-              <CheckboxField
-                labelProps={{ children: "Vegan / Vegetarian" }}
-                buttonProps={{
-                  ...getInputProps(signupPerson.alternativeMenu, {
-                    type: "checkbox",
-                  }),
-                }}
-                errors={signupPerson.alternativeMenu.errors}
-              />
-
-              <CheckboxField
-                labelProps={{ children: "Student" }}
-                buttonProps={{
-                  ...getInputProps(signupPerson.student, { type: "checkbox" }),
-                }}
-                errors={signupPerson.student.errors}
-              />
-
-              <Field
-                labelProps={{ children: "Dietary restrictions" }}
-                inputProps={{
-                  ...getInputProps(signupPerson.dietaryRestrictions, {
-                    type: "text",
-                  }),
-                }}
-                errors={signupPerson.dietaryRestrictions.errors}
-              />
-            </fieldset>
-          </li>
-          {people.map((person, index) => {
-            const { name, alternativeMenu, dietaryRestrictions, student } =
-              person.getFieldset();
-
-            return (
-              <li key={person.id} className="flex gap-3">
+            <ul className="flex flex-col gap-20">
+              <li className="flex gap-3">
                 <fieldset className="flex w-full flex-col gap-4">
                   <Field
                     labelProps={{ children: "Name" }}
-                    inputProps={{ ...getInputProps(name, { type: "text" }) }}
-                    errors={name.errors}
+                    inputProps={{
+                      ...getInputProps(signupPerson.name, { type: "text" }),
+                    }}
+                    errors={signupPerson.name.errors}
+                  />
+
+                  <Field
+                    labelProps={{ children: "Email" }}
+                    inputProps={{
+                      ...getInputProps(signupPerson.email, { type: "email" }),
+                    }}
+                    errors={signupPerson.email.errors}
+                  />
+
+                  <Field
+                    labelProps={{ children: "Phone number" }}
+                    inputProps={{
+                      ...getInputProps(signupPerson.phone, { type: "tel" }),
+                    }}
+                    errors={signupPerson.phone.errors}
                   />
 
                   <CheckboxField
                     labelProps={{ children: "Vegan / Vegetarian" }}
                     buttonProps={{
-                      ...getInputProps(alternativeMenu, { type: "checkbox" }),
+                      ...getInputProps(signupPerson.alternativeMenu, {
+                        type: "checkbox",
+                      }),
                     }}
-                    errors={alternativeMenu.errors}
+                    errors={signupPerson.alternativeMenu.errors}
                   />
 
                   <CheckboxField
                     labelProps={{ children: "Student" }}
                     buttonProps={{
-                      ...getInputProps(student, { type: "checkbox" }),
+                      ...getInputProps(signupPerson.student, {
+                        type: "checkbox",
+                      }),
                     }}
-                    errors={alternativeMenu.errors}
+                    errors={signupPerson.student.errors}
                   />
 
                   <Field
                     labelProps={{ children: "Dietary restrictions" }}
                     inputProps={{
-                      ...getInputProps(dietaryRestrictions, { type: "text" }),
-                    }}
-                    errors={dietaryRestrictions.errors}
-                  />
-
-                  <Button
-                    {...{
-                      ...form.remove.getButtonProps({
-                        name: fields.people.name,
-                        index,
+                      ...getInputProps(signupPerson.dietaryRestrictions, {
+                        type: "text",
                       }),
-                      disabled: people.length === 0,
                     }}
-                    variant="destructive"
-                  >
-                    Remove this person
-                  </Button>
+                    errors={signupPerson.dietaryRestrictions.errors}
+                  />
                 </fieldset>
               </li>
-            );
-          })}
-        </ul>
+              {people.map((person, index) => {
+                const { name, alternativeMenu, dietaryRestrictions, student } =
+                  person.getFieldset();
 
-        {people.length < 3 ? (
-          <Button
-            variant="outline"
-            {...form.insert.getButtonProps({ name: fields.people.name })}
-            className="mt-20"
-          >
-            Add a friend
-          </Button>
-        ) : null}
+                return (
+                  <li key={person.id} className="flex gap-3">
+                    <fieldset className="flex w-full flex-col gap-4">
+                      <Field
+                        labelProps={{ children: "Name" }}
+                        inputProps={{
+                          ...getInputProps(name, { type: "text" }),
+                        }}
+                        errors={name.errors}
+                      />
 
-        <ErrorList id={fields.people.id} errors={fields.people.errors} />
+                      <CheckboxField
+                        labelProps={{ children: "Vegan / Vegetarian" }}
+                        buttonProps={{
+                          ...getInputProps(alternativeMenu, {
+                            type: "checkbox",
+                          }),
+                        }}
+                        errors={alternativeMenu.errors}
+                      />
 
-        <TextareaField
-          labelProps={{ children: "Comment" }}
-          textareaProps={{ ...getTextareaProps(fields.comment) }}
-          errors={fields.comment.errors}
-        />
+                      <CheckboxField
+                        labelProps={{ children: "Student" }}
+                        buttonProps={{
+                          ...getInputProps(student, { type: "checkbox" }),
+                        }}
+                        errors={alternativeMenu.errors}
+                      />
 
-        <Button type="submit">Join</Button>
-      </Form>
+                      <Field
+                        labelProps={{ children: "Dietary restrictions" }}
+                        inputProps={{
+                          ...getInputProps(dietaryRestrictions, {
+                            type: "text",
+                          }),
+                        }}
+                        errors={dietaryRestrictions.errors}
+                      />
+
+                      <Button
+                        {...{
+                          ...form.remove.getButtonProps({
+                            name: fields.people.name,
+                            index,
+                          }),
+                          disabled: people.length === 0,
+                        }}
+                        variant="destructive"
+                      >
+                        Remove this person
+                      </Button>
+                    </fieldset>
+                  </li>
+                );
+              })}
+            </ul>
+
+            {people.length < 3 ? (
+              <Button
+                variant="outline"
+                {...form.insert.getButtonProps({ name: fields.people.name })}
+                className="mt-20"
+              >
+                Add a friend
+              </Button>
+            ) : null}
+
+            <ErrorList id={fields.people.id} errors={fields.people.errors} />
+
+            <TextareaField
+              labelProps={{ children: "Comment" }}
+              textareaProps={{ ...getTextareaProps(fields.comment) }}
+              errors={fields.comment.errors}
+            />
+
+            <Button type="submit">Join</Button>
+          </Form>
+        </>
+      )}
     </main>
   );
 }
