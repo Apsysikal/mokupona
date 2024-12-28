a4858a01ddc8836408dcc3ff325a09196692802f
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2024-12-28 08:55:00 +0000

    Working on about

---
+++ app/routes/about.tsx
@@ -0,0 +1,127 @@
+export default function AboutPage() {
+  const team = [
+    {
+      name: "Michael Foster",
+      description:
+        "Lorem ipsum odor amet, consectetuer adipiscing elit. Montes malesuada augue interdum feugiat volutpat? Varius ante magnis eu pharetra blandit. Lorem ipsum odor amet, consectetuer adipiscing elit. Montes malesuada augue interdum feugiat volutpat? Varius ante magnis eu pharetra blandit.",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+    },
+    {
+      name: "Dries Vincent",
+      description:
+        "Lorem ipsum odor amet, consectetuer adipiscing elit. Montes malesuada augue interdum feugiat volutpat? Varius ante magnis eu pharetra blandit.",
+      imageUrl:
+        "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+    },
+    {
+      name: "Lindsay Walton",
+      description:
+        "Lorem ipsum odor amet, consectetuer adipiscing elit. Montes malesuada augue interdum feugiat volutpat? Varius ante magnis eu pharetra blandit.",
+      imageUrl:
+        "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+    },
+    {
+      name: "Tom Cook",
+      description:
+        "Lorem ipsum odor amet, consectetuer adipiscing elit. Montes malesuada augue interdum feugiat volutpat? Varius ante magnis eu pharetra blandit.",
+      imageUrl:
+        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+    },
+    {
+      name: "Michael Foster",
+      description:
+        "Lorem ipsum odor amet, consectetuer adipiscing elit. Montes malesuada augue interdum feugiat volutpat? Varius ante magnis eu pharetra blandit. Lorem ipsum odor amet, consectetuer adipiscing elit. Montes malesuada augue interdum feugiat volutpat? Varius ante magnis eu pharetra blandit.",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+    },
+    {
+      name: "Dries Vincent",
+      description:
+        "Lorem ipsum odor amet, consectetuer adipiscing elit. Montes malesuada augue interdum feugiat volutpat? Varius ante magnis eu pharetra blandit.",
+      imageUrl:
+        "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+    },
+    {
+      name: "Lindsay Walton",
+      description:
+        "Lorem ipsum odor amet, consectetuer adipiscing elit. Montes malesuada augue interdum feugiat volutpat? Varius ante magnis eu pharetra blandit.",
+      imageUrl:
+        "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+    },
+    {
+      name: "Tom Cook",
+      description:
+        "Lorem ipsum odor amet, consectetuer adipiscing elit. Montes malesuada augue interdum feugiat volutpat? Varius ante magnis eu pharetra blandit.",
+      imageUrl:
+        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+    },
+  ];
+
+  return (
+    <main className="mx-auto mt-20 max-w-4xl px-2">
+      <div className="flex flex-col gap-8">
+        <h1 className="text-5xl lowercase">About Us</h1>
+        <p>
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Eu urna tincidunt
+          class amet mi. Metus proin rhoncus vitae massa venenatis maximus
+          conubia arcu. Nunc quisque quam id nullam cras euismod. Praesent curae
+          aenean urna; curae lobortis rhoncus primis? Quam pharetra congue
+          pulvinar finibus quisque habitasse metus lacinia. Ornare quisque
+          sapien diam; primis mollis massa. Venenatis congue laoreet cubilia
+          augue consequat suspendisse elementum fringilla diam. Tortor odio
+          torquent integer, cubilia ridiculus sagittis. Dapibus purus in
+          vehicula nulla diam. Vulputate vehicula proin ipsum mus iaculis
+          vulputate aliquam; ex urna. Elit inceptos porttitor ex mattis dolor
+          mauris porta tincidunt. Pharetra mollis massa penatibus mollis nullam
+          elementum ultrices euismod. Litora mus diam pretium a morbi; suscipit
+          molestie etiam quisque.
+        </p>
+        <p>
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Eu urna tincidunt
+          class amet mi. Metus proin rhoncus vitae massa venenatis maximus
+          conubia arcu. Nunc quisque quam id nullam cras euismod. Praesent curae
+          aenean urna; curae lobortis rhoncus primis? Quam pharetra congue
+          pulvinar finibus quisque habitasse metus lacinia. Ornare quisque
+          sapien diam; primis mollis massa. Venenatis congue laoreet cubilia
+          augue consequat suspendisse elementum fringilla diam. Tortor odio
+          torquent integer, cubilia ridiculus sagittis. Dapibus purus in
+          vehicula nulla diam. Vulputate vehicula proin ipsum mus iaculis
+          vulputate aliquam; ex urna. Elit inceptos porttitor ex mattis dolor
+          mauris porta tincidunt. Pharetra mollis massa penatibus mollis nullam
+          elementum ultrices euismod. Litora mus diam pretium a morbi; suscipit
+          molestie etiam quisque.
+        </p>
+      </div>
+
+      <div className="mt-20 flex flex-col gap-8">
+        <h2 className="text-4xl lowercase">Our Members</h2>
+        <ul className="grid grid-cols-1 gap-x-8 gap-y-16 text-center sm:grid-cols-2 md:grid-cols-4 lg:mx-0 lg:max-w-none">
+          {team.map((member) => (
+            <TeamMember key={member.name} {...member} />
+          ))}
+        </ul>
+      </div>
+    </main>
+  );
+}
+
+function TeamMember({
+  imageUrl,
+  name,
+  description,
+}: {
+  name: string;
+  imageUrl: string;
+  description: string;
+}) {
+  return (
+    <li>
+      <img alt="" src={imageUrl} className="mx-auto size-24 rounded-full" />
+      <h3 className="mt-6 text-base/7 font-semibold">{name}</h3>
+      <p className="text line-clamp-6 text-sm text-gray-50/80 hover:line-clamp-none">
+        {description}
+      </p>
+    </li>
+  );
+}
