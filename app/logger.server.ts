269bbc9943fe3d65c64473f1f9c018a63609e4fa
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2025-01-13 14:38:15 +0000

    Ignore logs

--- app/logger.server.ts
+++ app/logger.server.ts
@@ -7,8 +7,11 @@ const logger = winston.createLogger({
   format: winston.format.json(),
   transports: PRODUCTION
     ? [
-        new winston.transports.File({ filename: "error.log", level: "error" }),
-        new winston.transports.File({ filename: "combined.log" }),
+        new winston.transports.File({
+          filename: "./logs/error.log",
+          level: "error",
+        }),
+        new winston.transports.File({ filename: "./logs/combined.log" }),
         new winston.transports.Console({ format: winston.format.simple() }),
       ]
     : [
