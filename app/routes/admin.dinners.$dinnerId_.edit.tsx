3d6732c4d608d2a2948d01029f9c18c8004c9f2c
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2025-01-20 14:37:08 +0000

    Add logging

--- app/routes/admin.dinners.$dinnerId_.edit.tsx
+++ app/routes/admin.dinners.$dinnerId_.edit.tsx
@@ -32,6 +32,7 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
   await requireUserWithRole(request, ["moderator", "admin"]);
   const timeOffset = getTimezoneOffset(request);
   const timeZone = getTimezone(request);
+  logger.info(`Accept-Language: ${request.headers.get("Accept-Language")}`);
   const locale = (getClientLocales(request) || ["de-CH,de"])[0].split(",")[1];
 
   const { dinnerId } = params;
@@ -84,6 +85,7 @@ export async function action({ request, params }: ActionFunctionArgs) {
   const user = await requireUserWithRole(request, ["moderator", "admin"]);
   const timeOffset = getTimezoneOffset(request);
   const timeZone = getTimezone(request);
+
   const locale = (getClientLocales(request) || ["de-CH,de"])[0].split(",")[1];
 
   const { dinnerId } = params;
