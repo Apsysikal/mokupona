c6db6d07e5b2e49af3fee286c5f55b043eeb852c
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2026-03-13 13:15:22 +0000

    add @tailwindcss/vite and drop postcss plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import mdx from "@mdx-js/rollup";
 import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: { port: 3000 },
-  plugins: [mdx(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), mdx(), reactRouter(), tsconfigPaths()],
 });
